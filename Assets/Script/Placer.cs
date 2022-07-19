aa6cc70fdb69b36f2cae9ea072d2a215f7e0e67a
ang15 <32431798+ang15@users.noreply.github.com>
2022-07-19 14:28:17 +0300

    rename

---
+++ Assets/Script/Placer.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Script
+{
+    public class Placer : MonoBehaviour
+    {
+        public PlayerController player;
+        public PlacerBigOREnd ChunkPrefabs;
+        public PlacerBigOREnd FirstChunk;
+
+        public List<PlacerBigOREnd> spawnedChunks = new List<PlacerBigOREnd>();
+        void Start()
+        {
+            spawnedChunks.Add(FirstChunk);
+        }
+
+        void Update()
+        {
+            if (player.transform.position.x > spawnedChunks[spawnedChunks.Count - 1].End.position.x - 15)
+            {
+                player.monets += 5;
+                SpawnedChunks();
+            }
+        }
+
+        private void SpawnedChunks()
+        {
+            PlacerBigOREnd newChunk = Instantiate(ChunkPrefabs);
+            newChunk.transform.position = spawnedChunks[spawnedChunks.Count - 1].End.position - newChunk.Begin.localPosition;
+            spawnedChunks.Add(newChunk);
+
+            if (spawnedChunks.Count > 2)
+            {
+                Destroy(spawnedChunks[0].gameObject);
+                spawnedChunks.RemoveAt(0);
+            }
+
+        }
+
+        public void SetActive( bool active)
+        {
+            for(int i=0;i< spawnedChunks.Count;i++)
+            {
+                spawnedChunks[i].gameObject.SetActive(active);
+            }
+
+        }
+    }
+
+}
